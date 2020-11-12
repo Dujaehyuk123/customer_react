b5b5f2763151ceacf031389d4f1b7f4f9b302fa0
Dujaehyuk123 <hyouk070812@gmail.com>
2020-11-12 12:45:33 +0900

    submit data into databse

---
+++ client/src/components/CustomerAdd.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { post } from 'axios';
+
+class CustomerAdd extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            file: null,
+            userName: '',
+            birthday: '',
+            gender: '',
+            job: '',
+            fileName : ''
+        }
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault()
+        this.addCustomer()
+            .then((response) => {
+                console.log(response.data);
+            })
+    }
+    
+    handleFileChange = (e) => {
+        this.setState({
+            file: e.target.files[0],
+            fileName: e.target.value
+        })
+    }
+
+    handleValueChange = (e) => {
+        let nextState = {};
+        nextState[e.target.name] = e.target.value;
+        this.setState(nextState);
+    }
+
+    addCustomer = () => {
+        const url = '/api/customers';
+        const formData = new FormData();
+        formData.append('image', this.state.file)
+        formData.append('name', this.state.userName)
+        formData.append('birthday',this.state.birthday)
+        formData.append('gender',this.state.gender)
+        formData.append('job',this.state.job)
+        const config = {
+            headers: {
+                'content-type' : 'multipart/form-data'
+            }
+        }
+        return post(url, formData, config);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleFormSubmit}>
+                <h1>ADD CUSTOMER</h1>
+                PROFILE IMAGE: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.state.handleFileChange}/><br/>
+                NAME: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}/><br/>
+                BIRTHDAY: <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}/><br/>
+                GENDER: <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
+                job:  <input type="job" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>   
+                <button type="submit">Submit</button>
+            </form>
+        )
+    }
+
+}
+
+export default CustomerAdd;
